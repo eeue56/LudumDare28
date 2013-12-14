14f02ad09c9a164b4cda0eef3d881869a01f1fe8
eeue56 <eeue56@bangor.ac.uk>
2013-12-14 12:46:39 +0000

    adding main

---
+++ main.py
@@ -0,0 +1,104 @@
+#!/usr/bin/python2
+
+from __future__ import division
+
+# PyQT4 imports
+from PyQt4 import QtGui, QtCore, QtOpenGL
+from PyQt4.QtOpenGL import QGLWidget
+# PyOpenGL imports
+import OpenGL.GL as gl
+import OpenGL.arrays.vbo as glvbo
+
+from random import choice, randint
+from misc import *
+from world import World
+from world_objects import *
+
+
+class GLPlotWidget(QGLWidget):
+
+    def __init__(self, width, height, *args):
+        QGLWidget.__init__(self, *args)
+        self.width = width
+        self.height = height
+ 
+    def initializeGL(self):
+        """Initialize OpenGL, VBOs, upload data on the GPU, etc.
+        """
+        # background color
+        gl.glClearColor(0,0,0,0)
+        gl.glViewport(0, 0, self.width, self.height)
+        gl.glMatrixMode(gl.GL_PROJECTION)
+        gl.glLoadIdentity()
+
+    def paintGL(self):
+        """Paint the scene.
+        """
+        # clear the buffer
+        gl.glClear(gl.GL_COLOR_BUFFER_BIT)
+        
+        gl.glEnableClientState(gl.GL_VERTEX_ARRAY)
+        
+    def resizeGL(self, width, height):
+        """Called upon window resizing: reinitialize the viewport.
+        """
+        # update the window size    
+        self.width, self.height = width, height
+        # paint within the whole window
+        gl.glViewport(0, 0, self.width, self.height)
+        # set orthographic projection (2D only)
+        gl.glMatrixMode(gl.GL_PROJECTION)
+        gl.glLoadIdentity()
+        # the window corner OpenGL coordinates are the same as the world height and width
+        gl.glOrtho(0, self.world.width, 0, self.world.height, -1, 1)
+
+ 
+if __name__ == '__main__':
+    # import numpy for generating random data points
+    import sys
+
+ 
+    # define a QT window with an OpenGL widget inside it
+    class TestWindow(QtGui.QMainWindow):
+        def __init__(self):
+            super(TestWindow, self).__init__()
+            # initialize the GL widget
+
+            self.widget = GLPlotWidget(100, 100)
+            self.keys = set()
+
+            self.widget.setGeometry(0, 0, self.widget.width, self.widget.height)
+            self.setCentralWidget(self.widget)
+            self.show()
+
+            self.paint_timer = QtCore.QTimer()
+            QtCore.QObject.connect(self.paint_timer, QtCore.SIGNAL("timeout()"), self.widget.updateGL)
+            
+            self.button_timer = QtCore.QTimer()
+            QtCore.QObject.connect(self.button_timer, QtCore.SIGNAL("timeout()"), self.check)
+
+            QtCore.QMetaObject.connectSlotsByName(self)
+            
+            self.paint_timer.start(30)
+            self.button_timer.start(25)
+
+            self.resize(600, 400)
+
+        def keyPressEvent(self, event):
+            self.keys.add(event.key())
+
+        def keyReleaseEvent(self, event):
+            try:
+                self.keys.remove(event.key())
+            except:
+                pass
+
+        def check(self):
+            pass
+
+ 
+    # create the QT App and window
+    app = QtGui.QApplication(sys.argv)
+    window = TestWindow()
+    window.show()
+    app.exec_()
