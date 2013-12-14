f77325037fe5499ffa557f2e145ed5da98fd1892
eeue56 <eeue56@bangor.ac.uk>
2013-12-14 12:52:58 +0000

    adding setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='nigeludum',
+      version='1.0.0.6',
+      description='Ludum Dare game',
+      author='Enalicho',
+      author_email='enalicho@gmail.com',
+      packages=['nigeludum'],
+      requires=['PyQt', 'OpenGL'],
+     )
