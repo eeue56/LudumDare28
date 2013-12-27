3b0f79b3469111dd9f051c99dfab7dec8d0bd3b7
eeue56 <eeue56@bangor.ac.uk>
2013-12-27 13:52:24 +0000

    refactoring level parser

--- nigeludum/levels/level_parser.py
+++ nigeludum/levels/level_parser.py
@@ -7,8 +7,38 @@ from nigeludum.world_objects import Wall, known_objects
 def _color_dict_to_tuple(color):
     return color.values()
 
-def generate_objects(file_data):
+def _generate_fixed(level, fixed_records):
+    for object_, object_data in fixed_records.iteritems():
+        if object_ in known_objects:
+            if 'color' in object_data:
+                object_data['color'] = _color_dict_to_tuple(object_data['color'])
+            level.add_object(known_objects[object_](**object_data))
 
+def _generate_walls(world_width, world_height, wall_records):
+
+    wall_dict = {}
+    walls = []
+
+    Waller = lambda *a, **kw: Wall(world_width, world_height, *a, **kw)
+
+    for wall, wall_data in wall_records.iteritems():
+
+        direction = DIRECTIONS[wall]
+
+        if 'gaps' in wall_data:
+            gaps = range(wall_data['gaps']['start'], wall_data['gaps']['end'])
+        else:
+            gaps = []
+
+        width = wall_data['width']
+        wall_dict[direction] = wall_data['leads_to']
+
+        walls.append(Waller(width=width, facing=direction, gaps=gaps))
+
+    return wall_dict, walls
+
+
+def generate_objects(file_data):
     data = loads(file_data)
 
     level_dict = {}
@@ -16,38 +46,15 @@ def generate_objects(file_data):
     for level_id, level_data in data.iteritems():
         world_width = level_data['width']
         world_height = level_data['height']
-
-        wall_dict = {}
-        walls = []
-        for wall, wall_data in level_data['walls'].iteritems():
-
-            direction = DIRECTIONS[wall]
-
-            if 'gaps' in wall_data:
-                gaps = range(wall_data['gaps']['start'], wall_data['gaps']['end'])
-            else:
-                gaps = []
-
-            width = wall_data['width']
-            wall_dict[direction] = wall_data['leads_to']
-
-            walls.append(Wall(world_width, world_height, width=width, facing=direction, gaps=gaps))
-
         color = _color_dict_to_tuple(level_data['color'])
 
-        if level_id == "1":
-            print len(walls), wall_dict
+        wall_dict, walls = _generate_walls(world_width, world_height, level_data['walls'])
 
         level = Level(color, wall_dict, world_width, world_height)
         level.add_objects(walls)
+        _generate_fixed(level, level_data['fixed'])
 
         level_dict[int(level_id)] = level
-
-        for object_, object_data in level_data['fixed'].iteritems():
-            if object_ in known_objects:
-                if 'color' in object_data:
-                    object_data['color'] = _color_dict_to_tuple(object_data['color'])
-                level.add_object(known_objects[object_](**object_data))
 
     return level_dict
 
