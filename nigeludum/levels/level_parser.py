a2f4bc8f64f8e2eee061115bc23630ace6855cfb
eeue56 <eeue56@bangor.ac.uk>
2013-12-27 14:15:30 +0000

    initial random implmenetation

--- nigeludum/levels/level_parser.py
+++ nigeludum/levels/level_parser.py
@@ -1,5 +1,7 @@
 from json import dumps, loads
 
+from random import choice, randint
+
 from nigeludum.levels import Level
 from nigeludum.misc import *
 from nigeludum.world_objects import Wall, known_objects
@@ -13,6 +15,29 @@ def _generate_fixed(level, fixed_records):
             if 'color' in object_data:
                 object_data['color'] = _color_dict_to_tuple(object_data['color'])
             level.add_object(known_objects[object_](**object_data))
+
+## TODO: implement
+def _generate_random(level, random_records):
+    for object_class, object_data in random_records.iteritems():
+        if object_class not in known_objects:
+            print 'no such class as {c}'.format(c=object_class)
+            continue
+
+        position_information = object_data['between']
+
+
+        x = position_information['x']
+        y = position_information['y']
+        high_x = x + position_information['width']
+        high_y = y + position_information['height']
+        colors = [color.values() for color in object_data['colors']]
+
+        for _ in xrange(object_data['amount']):
+            my_x = randint(x, high_x)
+            my_y = randint(y, high_y)
+            my_color = choice(colors)
+
+            level.add_object(known_objects[object_class](my_x, my_y, my_color, 0))
 
 def _generate_walls(world_width, world_height, wall_records):
 
@@ -53,6 +78,9 @@ def generate_objects(file_data):
         level = Level(color, wall_dict, world_width, world_height)
         level.add_objects(walls)
         _generate_fixed(level, level_data['fixed'])
+
+        if 'random' in level_data:
+            _generate_random(level, level_data['random'])
 
         level_dict[int(level_id)] = level
 
