e8ed72ef51a019448fdd7b95f57c5673f5a454fe
eeue56 <eeue56@bangor.ac.uk>
2014-01-10 19:40:00 +0000

    addpending actions to mind

--- nigeludum/hivemind/mind.py
+++ nigeludum/hivemind/mind.py
@@ -3,4 +3,7 @@ class Mind(object):
 
     def __init__(self):
         self.action_paths = None
-        
+        self._actions = []
+
+    def register_action(self, action):
+        self._actions.append(action)
